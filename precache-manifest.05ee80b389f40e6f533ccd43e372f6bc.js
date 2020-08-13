e25000c2cfeeedee4933745dd931aef577281d11
saivamsivanamali <innovationaryradiant@gmail.com>
2020-08-13 16:55:45 +0530

    Updates

---
+++ precache-manifest.05ee80b389f40e6f533ccd43e372f6bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3ad0a33edddf62a15f4ac2eb03f94a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ef38385fc1551573278c",
+    "url": "/robofriends/static/css/main.8b5b9e4b.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef38385fc1551573278c",
+    "url": "/robofriends/static/js/main.f262f851.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "e91f73b8f684c3970edd9a230d6ce016",
+    "url": "/robofriends/static/media/BlackLivesMatter-Martinez.e91f73b8.woff"
+  }
+]);
